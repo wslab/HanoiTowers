f6f69c9494227fcaea0c3665c15a4326857714b9
alexk <akotov@gmail.com>
2019-10-23 00:24:43 -0700

    first commit

---
+++ HanioClassLibrary/Tower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HanoiClassLibrary
+{
+    public class Tower
+    {
+        public int numberOfDisks { get; set; }
+        public Tower(int n)
+        {
+            this.numberOfDisks = n;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HanoiClassLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Tower tower = new Tower(5);
+            Assert.AreEqual(tower.numberOfDisks, 5);
+        }
+    }
+}
